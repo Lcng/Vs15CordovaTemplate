c46ead6dea75e1010ea995166b6e9064310a6324
Lcng <294867413@qq.com>
2017-05-19 11:21:36 +0800

    1

---
+++ Vs15CordovaTemplate/Vs15CordovaTemplate.InJavaScript/www/scripts/index.js
@@ -0,0 +1,52 @@
+﻿var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        app.showStatusBar();
+        app.loadApp();
+    },
+    showStatusBar: function () {
+        if (StatusBar.show) {
+            StatusBar.show();
+        }
+
+        if (StatusBar.overlaysWebView) {
+            StatusBar.overlaysWebView(true);
+        }
+    },
+    loadApp: function () {
+        var isNetworkValid = app.checkConnection();
+        if (isNetworkValid == true) {
+            window.location.replace('http://59.63.8.244:3676/');
+        }
+        else {
+            window.location.replace('networkInvalid.html');
+        }
+    },
+    checkConnection: function () {
+        var networkState = navigator.network.connection.type;
+
+        if (networkState == Connection.UNKNOWN || networkState == Connection.NONE) {
+            return false;
+        }
+
+        return true;
+    }
+};
